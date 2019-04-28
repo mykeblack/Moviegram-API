dd4feef31e3d3462de7879d50eed598afba67dd7
myke black <mykeb@hotmail.com>
2019-04-28 12:54:07 +0100

    Added IMovieFactory factory class to return movie instances

---
+++ Moviegram.Domain/Interfaces/IMovieFactory.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Moviegram.Domain.Interfaces
+{
+    public class IMovieFactory
+    {
+
+        // creates an instance of the movie using a generic class
+        public IMovie GetMovie() {
+            var movie = new IMovie();
+            return movie;
+        }
+
+        public IMovie GetMovie(int movieId) {
+            var movie = new IMovie(movieId);
+            return movie;
+        }
+
+        public List<IMovie> GetMovies() {
+            var list = new List<IMovie>();
+            return list;
+        }
+    }
+}
