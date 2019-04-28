3480b1d987e0fc1d6a1f344c385c7e4ba8ec349e
myke black <mykeb@hotmail.com>
2019-04-28 17:48:27 +0100

    worked on api side of things. added try-catch on public methods, changed moviefactory to use Using() and return error message when errors are caught.

--- Moviegram API/Controllers/MovieController.cs
+++ Moviegram API/Controllers/MovieController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Moviegram.Domain;
 
 namespace Moviegram_API.Controllers
 {
@@ -18,24 +19,44 @@ namespace Moviegram_API.Controllers
         [HttpGet]
         public ActionResult<string> Get(string title, int limit, string keyword, string startdate, string enddate)
         {
-            var mf = new Moviegram.Domain.MovieFactory();
-            var list = mf.GetMovies(title, limit, keyword, startdate, enddate);
-            return Newtonsoft.Json.JsonConvert.SerializeObject(list);
+            try
+            {
+                using (var mf = new MovieFactory())
+                {
+                    var list = mf.GetMovies(title, limit, keyword, startdate, enddate);
+                    return Newtonsoft.Json.JsonConvert.SerializeObject(list);
+                }
+            }
+            catch (Exception ex) {
+                // we could a custom error object to return a json safe error message, but will just create as a string for now.
+                return "{error: " + ex.Message + "}";
+            }
         }
 
-        // GET api/values/5
+        // returns details of a single movie
         [Route("/movies/detail")]
         [HttpGet("{id}")]
         public ActionResult<string> Get(int id)
         {
-            return "value";
+            try
+            {
+                using (var mf = new MovieFactory())
+                {
+                    var movie = mf.GetMovie(id);
+                    return Newtonsoft.Json.JsonConvert.SerializeObject(movie);
+                }
+            }
+            catch (Exception ex)
+            {
+                return "{error: " + ex.Message + "}";
+            }
         }
 
 
 
-        // ADMIN methods
-        // we're not building this right now, so just return unauthorised message because we have not built in authentication yet.
 
+        // ADMIN methods
+        // we're not building this part of the application right now, so just return unauthorised message because we have not built in authentication yet.
         [Authorize]
         [Route("/movies/add")]
         [HttpPost]
